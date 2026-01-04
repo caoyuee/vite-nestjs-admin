a8604a29f358d0f6e5a00567604d4d396d5e9bc9
caoyuee <chencaoyu5257@163.com>
2026-01-04 08:51:20 +0800

    fix:"修复3D"

--- src/views/webgpu/index.vue
+++ src/views/webgpu/index.vue
@@ -4,19 +4,9 @@
   </div>
 </template>
 <script setup lang="ts" name="webgpu">
-import {
-  Engine3D,
-  Scene3D,
-  Object3D,
-  Camera3D,
-  View3D,
-  LitMaterial,
-  BoxGeometry,
-  MeshRenderer,
-  DirectLight,
-  HoverCameraController,
-  AtmosphericComponent
-} from "@orillusion/core";
+import { Engine3D, View3D, Scene3D, CameraUtil, AtmosphericComponent, webGPUContext, HoverCameraController, Object3D, DirectLight, KelvinUtil, LitMaterial, MeshRenderer, PostProcessingComponent, BitmapTexture2D, GlobalFog, Color } from '@orillusion/core';
+import { TerrainGeometry } from '@orillusion/geometry';
+import { Stats } from '@orillusion/stats';
 import { ref, onMounted } from "vue";
 const canvas = ref<HTMLCanvasElement>();
 onMounted(() => {
@@ -27,47 +17,67 @@ const initEngine3D = async () => {
   await Engine3D.init({
     canvasConfig: { canvas: canvas.value }
   });
-  let scene3D = new Scene3D();
-  // 添加大气散射天空组件
-  let sky = scene3D.addComponent(AtmosphericComponent);
-  // 新建摄像机实例
-  let cameraObj = new Object3D();
-  let camera = cameraObj.addComponent(Camera3D);
-  // 根据窗口大小设置摄像机视角
-  camera.perspective(60, window.innerWidth / window.innerHeight, 1, 5000.0);
-  // 设置相机控制器
-  let controller = camera.object3D.addComponent(HoverCameraController);
-  controller.setCamera(0, 0, 15);
-  // 添加相机节点
-  scene3D.addChild(cameraObj);
-  // 新建光照
-  let light = new Object3D();
-  // 添加直接光组件
-  let component = light.addComponent(DirectLight);
-  // 调整光照参数
-  light.rotationX = 45;
-  light.rotationY = 30;
-  component.intensity = 2;
-  // 添加光照对象
-  scene3D.addChild(light);
-  // 新建对象
-  const obj = new Object3D();
-  // 为对象添 MeshRenderer
-  let mr = obj.addComponent(MeshRenderer);
-  // 设置几何体
-  mr.geometry = new BoxGeometry(5, 5, 5);
-  // 设置材质
-  mr.material = new LitMaterial();
-  scene3D.addChild(obj);
-  // 创建View3D对象
   let view = new View3D();
-  // 指定渲染的场景
-  view.scene = scene3D;
-  // 指定使用的相机
-  view.camera = camera;
-  // 开始渲染
+  view.scene = new Scene3D();
+  view.scene.addComponent(AtmosphericComponent);
+  view.scene.addComponent(Stats);
+
+  view.camera = CameraUtil.createCamera3DObject(view.scene);
+  view.camera.perspective(60, webGPUContext.aspect, 1, 50000.0);
+  view.camera.object3D.z = -15;
+  view.camera.object3D.addComponent(HoverCameraController).setCamera(35, -20, 10000);
+
   Engine3D.startRenderView(view);
+
+  let post = view.scene.addComponent(PostProcessingComponent);
+  let fog = post.addPost(GlobalFog);
+  fog.start = 2000;
+  fog.end = 0;
+  fog.fogHeightScale = 0.116;
+  fog.density = 0.094;
+  fog.ins = 0.1041;
+  fog.skyFactor = 0.35;
+  fog.overrideSkyFactor = 0.7;
+
+  fog.fogColor = new Color(136 / 255, 215 / 255, 236 / 255, 1);
+  fog.fogHeightScale = 0.1;
+  fog.falloff = 0.626;
+  fog.scatteringExponent = 8;
+  fog.dirHeightLine = 6.5;
+
+  createScene(view.scene);
 };
+
+const createScene = async (scene: Scene3D) => {
+  {
+    let sunObj = new Object3D();
+    let sunLight = sunObj.addComponent(DirectLight);
+    sunLight.lightColor = KelvinUtil.color_temperature_to_rgb(6553);
+    sunLight.castShadow = true;
+    sunLight.intensity = 4;
+    sunObj.transform.rotationX = 50;
+    sunObj.transform.rotationY = 50;
+    scene.addChild(sunObj);
+  }
+
+  //bitmap
+  let bitmapTexture = await Engine3D.res.loadTexture('https://cdn.orillusion.com/terrain/test01/bitmap.png');
+  let heightTexture = await Engine3D.res.loadTexture('https://cdn.orillusion.com/terrain/test01/height.png');
+  let terrainSizeW = 20488;
+  let terrainSizeH = 20488;
+  let terrainGeometry: TerrainGeometry;
+  {
+    let mat = new LitMaterial();
+    terrainGeometry = new TerrainGeometry(terrainSizeW, terrainSizeH, 2000, 2000);
+    terrainGeometry.setHeight(heightTexture as BitmapTexture2D, 5000);
+    let floor = new Object3D();
+    let mr = floor.addComponent(MeshRenderer);
+    mr.geometry = terrainGeometry;
+    mat.baseMap = bitmapTexture;
+    mr.material = mat;
+    scene.addChild(floor);
+  }
+}
 </script>
 <style lang="scss" scoped>
 @use "./index";
